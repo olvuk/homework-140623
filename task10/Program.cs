78b073df8b1640d8e0aa95b83454845c79136965
Olga <radenovic-family@yandex.ru>
2023-06-14 21:20:19 +0200

    Added tesk 10

---
+++ task10/Program.cs
@@ -0,0 +1,16 @@
+﻿int getNumber()
+{
+    Console.WriteLine("Введите, пожалуйста, число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int print2Number(int number1)
+{
+    int num = (number1/10) % 10;
+    return num;
+}
+
+int number1 = getNumber();
+int number = print2Number(number1);
+Console.WriteLine(number);
