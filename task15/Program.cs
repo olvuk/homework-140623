5985ec23d3c2a4b458787bb169afb39fb8bbc23d
Olga <radenovic-family@yandex.ru>
2023-06-14 21:25:01 +0200

    Added task 15

---
+++ task15/Program.cs
@@ -0,0 +1,21 @@
+﻿int getDayNumber()
+{
+    Console.WriteLine("Введите, пожалуйста, число от 1 до 7");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void checkDay(int dayNumber)
+{
+    if (dayNumber >= 1 && dayNumber <= 5)
+    {
+        Console.WriteLine("Этот день не является выходным");
+    }
+    else 
+    {
+         Console.WriteLine("Этот день является выходным");
+    }
+}
+
+int dayNumber = getDayNumber();
+checkDay(dayNumber);
