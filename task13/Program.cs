75372aebdc8ac9191746f6f616ae91dab4bc5a5e
Olga <radenovic-family@yandex.ru>
2023-06-14 21:22:36 +0200

    Added task 13

---
+++ task13/Program.cs
@@ -0,0 +1,28 @@
+﻿int getNewNumber()
+{
+    Console.WriteLine("Введите, пожалуйста, число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void print3Number(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+       while (number > 999)
+       {
+        number = number / 10;
+       }
+       int thirdNumber = number % 10;
+
+       Console.WriteLine(thirdNumber);
+    }
+
+}
+
+int number = getNewNumber();
+print3Number(number);
